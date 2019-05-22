5180a8476b3ab3f3ba1ba220e6618fc697504da8
Philipe Sena Saviott <psaviott@hotmail.com>
2019-05-22 18:27:51 -0300

    feat: create app.route functions

--- application.py
+++ application.py
@@ -1,21 +1,24 @@
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 # Create the appropriate app.route functions,
 
 # Create the app.route functions when the user sends the URI "/category"
 @app.route('/category')
 def catalogFunction():
-    return "Works!"
+    if request.method == 'GET':
+        return render_template('catalog.html')
 
 # Create the app.route functions when the user sends the URI "/category/category_id/items"
 @app.route('/category/<int:category_id>/items')
 def categoryFunction(category_id):
-    return "Works too!"
+    if request.method == 'GET':
+        return render_template('category.html')
 
 # Create the app.route functions when the user sends the URI "/category/category_id/item_id"
 @app.route('/category/<int:category_id>/<int:item_id>')
 def itemFunction(category_id, item_id):
-    return "Works too too!"
+    if request.method == 'GET':
+        return render_template('item.html')
 
 
 if __name__ == '__main__':
